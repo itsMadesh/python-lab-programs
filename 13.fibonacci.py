5b45583c4bc1af6d91be7f1372b546b063a1656e
Madeshwaran A <madeshwaran.anandharaj@gmail.com>
2020-12-23 18:28:25 +0530

    optimized the  fibonacci logic

--- 13.fibonacci.py
+++ 13.fibonacci.py
@@ -2,14 +2,9 @@ n = int(input("Enter a number of terms :"))
 if(n < 1):
     print("invalid input")
 s = 0
-a = 0
+a = -1
 b = 1
-arr = []
-if(n >= 1):
-    print(0, end=" ")
-if(n >= 2):
-    print(1, end=" ")
-for i in range(2, n):
+for i in range(0, n):
     s = a+b
     print(s, end=" ")
     a = b
