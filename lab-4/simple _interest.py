7abc4d8c72cb1994ad19a127adfa6856c8e70ae6
Madeshwaran A <madeshwaran.anandharaj@gmail.com>
2021-01-07 12:52:08 +0530

    lab-4 programs added

---
+++ lab-4/simple _interest.py
@@ -0,0 +1,5 @@
+p = int(input("Enter the principal amount:"))
+t = int(input("Enter the time :"))
+r = int(input("Enter the rate of interest:"))
+s = (p*t*r)/100
+print("simple interest is", s)
---
+++ lab-4/solve_quadratic_equation.py
@@ -0,0 +1,11 @@
+import math
+a = int(input("Enter a number a :"))
+b = int(input("Enter a number b :"))
+c = int(input("Enter a number c :"))
+d = b*b-4*a*c
+if(d >= 0):
+    root1 = (-b+math.sqrt(d))/2*a
+    root2 = (-b-math.sqrt(d))/2*a
+    print("roots are", root1, root2)
+else:
+    print("roots are imaginary")
