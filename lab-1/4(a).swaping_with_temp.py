adfde72943415b92bc56fc000025899159c746da
Madeshwaran A <madeshwaran.anandharaj@gmail.com>
2021-01-30 03:41:25 +0530

    renamed lab-1 4a and 4b programs

--- lab-1/4(a).swaping_with_temp.py
+++
@@ -1,7 +0,0 @@
-# This program will swap two numbers using temporary varaible.
-a = input()
-b = input()
-t = a
-a = b
-b = t
-print("The swaped values are ", a, b)
--- lab-1/4(b).swaping_without_temp.py
+++
@@ -1,8 +0,0 @@
-# This program will swap two numbers without temporary varaible.
-
-a = int(input("enter a first number :"))
-b = int(input("enter a second number :"))
-a = a+b
-b = a-b
-a = a-b
-print("The swaped values are ", a, b)
