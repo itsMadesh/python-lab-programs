80186486bd1ab97357e58254a1c24b462aad0b1e
Madeshwaran A <madeshwaran.anandharaj@gmail.com>
2021-03-26 19:14:25 +0530

    Include one print statement

--- lab-5/2.prime_numbers.py
+++ lab-5/2.prime_numbers.py
@@ -1,7 +1,8 @@
 n = int(input("Enter a limit:"))
+print("Prime numbers between 1 to",n,":")
 for i in range(2, n+1):
     for j in range(2, i):
         if(i % j == 0):
             break
     else:
-        print(i)
+        print(i,end=" ")
